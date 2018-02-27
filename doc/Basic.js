5510bb02f0dbb024ace46287f2218e991358bf15
Grifree <1149290648@qq.com>
2018-02-27 14:55:04 +0800

    basic

--- doc/Basic.js
+++ doc/Basic.js
@@ -1,12 +1,45 @@
 var React = require('react')
 var Radio = require('radio.react')
 class Basic extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            basic: 'a'
+        }
+    }
     render () {
+        let self = this
         return (
             <div className="basicDemo" >
-	            <label>
-	                <Radio />
-	                basic
+                <label>
+                    <Radio  checked={self.state.basic === 'a'} 
+                            onChange={function (e) {
+                                self.setState({
+                                    basic: 'a'
+                                })
+                            }} 
+                    />
+                    <span>radio</span>
+                </label>
+                <label>
+                    <Radio checked={self.state.basic === 'b'} 
+                            onChange={function (e) {
+                                self.setState({
+                                    basic: 'b'
+                                })
+                            }} 
+                    />
+                    <span>radio</span>
+                </label>
+                <hr/>
+                <label>
+                    <Radio checked={self.state.disabledChecked} disabled={true} />
+                    <span>disabled | checked:true</span>
+                </label>
+                <hr/>
+                <label>
+                    <Radio checked={self.state.disabledFalseChecked} disabled={true} />
+                    <span>disabled | checked: false</span>
                 </label>
             </div>
         )
@@ -14,3 +47,4 @@ class Basic extends React.Component {
 }
 /*ONFACE-DEL*/Basic = require("react-hot-loader").hot(module)(Basic)
 module.exports = Basic
+
