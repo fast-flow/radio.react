72b19cce9df0054a1c2e2b66a65e93c6a11ef923
Grifree <1149290648@qq.com>
2018-05-09 10:29:36 +0800

    onface/module auto-generated commit

---
+++ 0.2.5/lib/index.js
@@ -0,0 +1,18 @@
+import { Component } from "react"
+import extend from "extend"
+import util from "util.react"
+import Checkbox from 'checkbox.react'
+require('./index.css')
+
+class Radio extends Component {
+	static RadioGroup = require('./RadioGroup').default
+    render() {
+        const self = this
+        return (
+            <Checkbox {...self.props} />
+        )
+    }
+}
+require('./props').default(Radio)
+export default Radio
+module.exports= Radio
